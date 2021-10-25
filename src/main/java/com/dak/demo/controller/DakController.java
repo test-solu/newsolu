6eb079315f92850706e412d2e3b9cbb88e941576
byube <kangjunne@naver.com>
2021-10-25 23:15:38 +0900

    빠른작업 환경 개선

--- src/main/java/com/dak/demo/controller/DakController.java
+++ src/main/java/com/dak/demo/controller/DakController.java
@@ -1,11 +1,16 @@
 package com.dak.demo.controller;
 
+import com.dak.demo.service.DakMainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DakController {
+
+	@Autowired
+	private DakMainService service;
 	
 	@RequestMapping(value = "/hello")
 	public ModelAndView hello() throws Exception{
